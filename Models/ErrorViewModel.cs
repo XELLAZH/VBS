68b80f98f2e2027a85fe5f9fb35c195ee33f89da
XELLAZH <zhuchkovsasha2004@yandex.ru>
2023-05-10 06:12:25 +0300

    Start from scratch

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VBS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
