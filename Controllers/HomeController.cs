88e0123f911aea2263f00749589ca2156a35b3e1
XELLAZH <zhuchkovsasha2004@yandex.ru>
2023-05-10 06:09:10 +0300

    First tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,20 +8,13 @@ namespace VBS.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
+
+        public IActionResult About() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
